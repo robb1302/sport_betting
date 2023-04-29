13f63a9416302241c94190a3e9df9e1e6c634c33
Robert Gerth <robert.gerth@mail.de>
2023-04-29 10:34:57 +0200

    TotalLossValue hinzugefügt für Klassifizierungsprobleme

--- src/models/loss_function.py
+++ src/models/loss_function.py
@@ -12,8 +12,25 @@ class PrecisionLoss(nn.Module):
 
         output = torch.clamp(output,0,1)>0.5
         target = torch.clamp(target,0,1)>0.5
-        # loss = torch.sum(torch.abs(self.X_train_subset) * (torch.abs(target)-torch.abs(output)))/ torch.sum(torch.abs(self.X_train_subset))
         precision = torch.sum((target == True) & (output == True))/(torch.sum( output == True)+1)
         loss = 1-precision
+        
+        return loss
+    
+
+class TotalValueLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, output, target):
+        if len(output.shape)>1:
+            output = output[:,1]
+
+        output = torch.clamp(output,0,1)>0.5
+        target = torch.clamp(target,0,1)>0.5
+        right_picks = torch.sum((target == True) & (output == True))
+        wrong_picks = torch.sum((target == False) & (output == True))
+       
+        loss = wrong_picks - right_picks
         
         return loss
