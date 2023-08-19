147dd4616b297d6f7c29a448221837789c74c7db
Robert Gerth <robert.gerth@mail.de>
2023-08-19 17:40:52 +0200

    fixed create new league

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ DATA_FOLDER_MODELS = DATA_FOLDER + '/models/'
 NUMBER_OF_GAMES = 3
 
 MAIN_LEAGUES = ["D1", "F1", "I1", "E0", "E1", "E2", "E3", "SP1", "D2", "F2", "SP2", "I2", "N1", "SC0", "B1", "P1", "T1", "G1"]
-SEASONS =  ["1213","1314","1415","1516","1617","1718","1819","1920","2021","2122","2223"]
+SEASONS =  ["1213","1314","1415","1516","1617","1718","1819","1920","2021","2122","2223","2324"]
 ATTRIBUTES_MAIN_LEAGUE = ['AC','AF','AHCh','AHh','AR','AS','AST','AY','Avg<2.5','Avg>2.5','AvgA','AvgAHA','AvgAHH','AvgC<2.5','AvgC>2.5','AvgCA','AvgCAHA','AvgCAHH','AvgCD','AvgCH','AvgD','AvgH','AwayTeam','B365<2.5','B365>2.5','B365A','B365AHA','B365AHH','B365C<2.5','B365C>2.5','B365CA','B365CAHA','B365CAHH','B365CD','B365CH','B365D','B365H','BWA','BWCA','BWCD','BWCH','BWD','BWH','Date','Div','FTAG','FTHG','FTR','HC','HF','HR','HS','HST','HTAG','HTHG','HTR','HY','HomeTeam','IWA','IWCA','IWCD','IWCH','IWD','IWH','Max<2.5','Max>2.5','MaxA','MaxAHA','MaxAHH','MaxC<2.5','MaxC>2.5','MaxCA','MaxCAHA','MaxCAHH','MaxCD','MaxCH','MaxD','MaxH','P<2.5','P>2.5','PAHA','PAHH','PC<2.5','PC>2.5','PCAHA','PCAHH','PSA','PSCA','PSCD','PSCH','PSD','PSH','Time','VCA','VCCA','VCCD','VCCH','VCD','VCH','WHA','WHCA','WHCD','WHCH','WHD','WHH']
 
 EXTRA_LEAGUES = ["ARG", "AUT", "BRA", "CHN", "DNK", "FIN", "IRL", "JPN", "MEX", "NOR", "POL", "ROU", "RUS", "SWE", "SWZ", "USA"]
--- scripts/01_provide_data/create_new_league.py
+++ scripts/01_provide_data/create_new_league.py
@@ -1,22 +1,27 @@
-import sys
 import os
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+import sys
+import tqdm
+
+
+# Get the current working directory
+current_directory = os.getcwd()
+# Define the substring to find
+substring_to_find = 'sport_betting'
+# Use str.find() to find the position of the substring
+index = current_directory.find(substring_to_find)
+parent_dir = f"{ current_directory[0:index]}/{substring_to_find}"
 sys.path.append(parent_dir)
 
 from src.utils.utils import createDirs
 import config as CONFIG
 from src.data.download import downloadLeague
 
-
-LIGA = ["D1", "F1", "I1", "E0", "E1","E2","E3", "SP1", "D2", "F2", "SP2", "I2", "N1", "SC0", "B1", "P1", "T1", "G1"]
-SEASONS = ["2223"]
-
-for liga in CONFIG.MAIN_LEAGUES:
+for liga in tqdm.tqdm(CONFIG.MAIN_LEAGUES):
     createDirs(liga=liga, data_path=CONFIG.DATA_FOLDER_RAW)
     for s in CONFIG.SEASONS:
         try:
             downloadLeague(liga=liga, season = s)
-        except:
+        except Exception as e:
+            print("Error:"e,"\n",liga,s)
             pass
-#downloadLineupsLeague(1,300,LIGA)
-#downloadLeagueValue(yearStart=2014,yearEnd = 2020, Ligen=["E0"])
+
