d5fb5e5b37a1887dfa3b0f1535c9ee27ba5115c0
Robert Gerth <robert.gerth@mail.de>
2023-08-29 19:41:59 +0200

    update streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,6 @@ if st.button('Getting Startetd'):
 # download_fixtures()
 
 # Read the CSV file into a DataFrame
-# df = pd.read_csv("data/fixtures/update.csv",index_col=0)[['Div','HomeTeam','AwayTeam']]
 df = pd.read_csv("data/results/results.csv",index_col=0)
 
 # Display the DataFrame as a table
