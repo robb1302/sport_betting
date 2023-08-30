b8e9ca7e4f2342815fb08f4b5c60724ac74e28bf
Robert Gerth <robert.gerth@mail.de>
2023-08-30 22:01:36 +0200

    changed to worklfow download

--- scripts/batch_runs/workflow_update_fixtures.bat
+++ scripts/batch_runs/workflow_update_fixtures.bat
@@ -5,7 +5,7 @@ rem Activate the virtual environment if needed (adjust the path accordingly)
 call D:\a\sport_betting\.venv\Scripts\activate.bat
 
 rem Execute your Python script within the virtual environment
-py ..\provide_data\download_fixtures.py 
+py ..\workflows\update_fixtures_workflow.py
 
 rem Deactivate the virtual environment (if activated)
 call D:\a\sport_betting\.venv\Scripts\deactivate.bat
