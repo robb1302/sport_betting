d59a381b5ef40b9f89a5c9ae40ad0b7d27182d00
Robert Gerth <robert.gerth@mail.de>
2023-08-30 22:19:12 +0200

    add prints for debug

--- scripts/workflows/update_fixtures_workflow.py
+++ scripts/workflows/update_fixtures_workflow.py
@@ -7,8 +7,10 @@ import logging
 if __name__ == "__main__":
 
     # Configure logging
-    print(os.getcwd())
+    print("File:",os.getcwd())
+    print("Dirs",os.listdir())
     # Create a StreamHandler to also log to the console
-    
+    print("Import...")
     from src.data.download import download_fixtures
+    print("Download...")
     download_fixtures(url="https://www.football-data.co.uk/fixtures.csv")
