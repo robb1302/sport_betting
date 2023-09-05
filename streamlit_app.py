804d5fba07d10016c77916d14ed094de000ec5f6
Robert Gerth <robert.gerth@mail.de>
2023-09-05 20:09:01 +0200

    dark theme test

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,31 @@ import os
 import streamlit_analytics
 
 streamlit_analytics.start_tracking()
-st.header('Cutting Edge Sports Prediction ⚽!')
-if st.button('Getting Startetd'):
-    st.balloons()
+
+
+
+# Center-align the title using HTML and CSS style
+st.write(
+    """
+    <div style="text-align: center;">
+        <h2> Welcome to </h2>
+        <h1>🍀The Gamblin' Goblin🍀</h1>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+# if st.button('Getting Started'):
+#     st.balloons()
+
 # Path to your local CSV file
 # download_fixtures()
 
 # Read the CSV file into a DataFrame
-df = pd.read_csv("data/results/results.csv",index_col=0)
+df = pd.read_csv("data/results/results.csv", index_col=0)
 
 # Display the DataFrame as a table
-st.write("Upcoming Games")
+# st.write("Upcoming Games")
 
 # Sidebar filter options
 st.sidebar.header('Filter League')
@@ -28,4 +42,5 @@ filtered_df = df[df['Div'] == selected_div]
 
 # Display the filtered DataFrame
 st.dataframe(filtered_df, hide_index=True)
+
 streamlit_analytics.stop_tracking()
