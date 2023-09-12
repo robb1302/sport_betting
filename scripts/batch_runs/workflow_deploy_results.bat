f0ad98351ecf07627876aa1b2191433adb219080
Robert Gerth <robert.gerth@mail.de>
2023-09-12 18:21:42 +0200

    add workflow_update_fixtures

---
+++ scripts/batch_runs/workflow_deploy_results.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+
+rem Activate the virtual environment if needed (adjust the path accordingly)
+call D:\a\sport_betting\.venv\Scripts\activate.bat
+
+rem Execute your Python script within the virtual environment
+py .\scripts\workflows\deploy_results_workflow.py
+
+rem Deactivate the virtual environment (if activated)
+call D:\a\sport_betting\.venv\Scripts\deactivate.bat
+
+rem End of script
+exit /b 0
